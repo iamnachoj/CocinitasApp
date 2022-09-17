80d723d574a73bde8475e54a60223fe8f1de5f21
Ignacio Jimenez <nachojimenez.94@gmail.com>
2022-09-17 18:26:41 +0200

    add recipe html structure

--- src/app/components/recipe-list/recipe/recipe.component.ts
+++ src/app/components/recipe-list/recipe/recipe.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe',
-  templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.css']
+    selector: 'app-recipe',
+    templateUrl: './recipe.component.html',
+    styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
+    recipes: Recipe[] = [new Recipe(
+        'Solomillo al Whisky',
+        'El solomillo al whisky es una de las recetas más conocidas en los bares de tapas de Sevilla. Normalmente se acompaña con unas ricas patatas fritas pero puedes acompañarlo igualmente con una buena ensalada o con arroz blanco y tienes un plato estupendo para tu comida del día a día.',
+        'https://yhoyquecomemos.com/wp-content/uploads/2022/01/receta-de-solomillo-d-cerdo-al-whisky.jpg.webp'
+      )
+    ]
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
