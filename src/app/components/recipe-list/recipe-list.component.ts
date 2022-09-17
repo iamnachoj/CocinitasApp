f67e06a9735665696bc1ffbd104ee98db61458b1
Ignacio Jimenez <nachojimenez.94@gmail.com>
2022-09-17 18:28:34 +0200

    move object recipe to recipe list from recipe component

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "./recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [new Recipe(
+      'Solomillo al Whisky',
+      'El solomillo al whisky es una de las recetas más conocidas en los bares de tapas de Sevilla. Normalmente se acompaña con unas ricas patatas fritas pero puedes acompañarlo igualmente con una buena ensalada o con arroz blanco y tienes un plato estupendo para tu comida del día a día.',
+      'https://yhoyquecomemos.com/wp-content/uploads/2022/01/receta-de-solomillo-d-cerdo-al-whisky.jpg.webp'
+  )
+  ]
   constructor() { }
 
   ngOnInit(): void {
