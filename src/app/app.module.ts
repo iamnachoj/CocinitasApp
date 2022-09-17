5528b46cc43421faa0111ee6907eef56af1f8b5d
Ignacio Jimenez <nachojimenez.94@gmail.com>
2022-09-17 17:26:50 +0200

    add folders from existing started project

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {ShoppingListComponent} from "./components/shopping-list/shopping-list-component";
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeComponent } from './components/recipe-list/recipe/recipe.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingItemComponent } from './components/shopping-list/shopping-item/shopping-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    RecipeComponent,
+    HeaderComponent,
+    ShoppingListEditComponent,
+    ShoppingItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
