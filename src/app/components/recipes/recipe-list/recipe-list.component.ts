063dc604fe10051a0bb164f4c0093f84128c66da
Ignacio Jimenez <nachojimenez.94@gmail.com>
2022-09-20 20:26:44 +0200

    worked on recipe-details component q
    none

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -9,10 +9,10 @@ import {Recipe} from "./recipe.model";
 export class RecipeListComponent implements OnInit {
     recipes: Recipe[] = [new Recipe(
         'Solomillo al Whisky',
-        'El solomillo al whisky es una de las recetas más conocidas en los bares de tapas de Sevilla. Normalmente se acompaña con unas ricas patatas fritas pero puedes acompañarlo igualmente con una buena ensalada o con arroz blanco y tienes un plato estupendo para tu comida del día a día.',
+        'Una de las recetas más conocidas en los bares de tapas de Sevilla. Normalmente se acompaña con patatas fritas.',
         'http://estoyhechouncocinillas.com/wp-content/uploads/2022/04/solomillo-al-whisky-sevillano.jpg'
     ),
-    new Recipe('Gazpacho Andaluz', 'El gazpacho es una sopa fría con varios ingredientes como aceite de oliva, vinagre, agua, hortalizas crudas, generalmente tomates, pepinos, pimientos y ajo.', 'https://www.acouplecooks.com/wp-content/uploads/2021/07/Gazpacho-002s.jpg')]
+    new Recipe('Gazpacho Andaluz', 'Sopa fría con varios ingredientes como aceite de oliva, vinagre, agua, hortalizas crudas, generalmente tomates, pepinos, pimientos y ajo.', 'https://www.acouplecooks.com/wp-content/uploads/2021/07/Gazpacho-002s.jpg')]
 
     constructor() {
     }
